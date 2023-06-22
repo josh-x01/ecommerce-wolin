ba63407eff8007d69c414c9c7e171218a1a12b6f
unknown <bezawit89>
2023-06-22 15:20:09 +0300

    my ecommerce

---
+++ backend/data.js
@@ -0,0 +1,73 @@
+import bcrypt from 'bcryptjs';
+const data = {
+  users: [
+    {
+      name: 'wase',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'Jhon',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
+  products: [
+    {
+      //_id: '1',
+      name: ' mukash habesha kemis',
+      slug: 'mukash-habesha-kemis',
+      category: 'kemis',
+      image: '/images/p1.jpg',
+      price: 500,
+      countInStock: 10,
+      brand: 'mukash fashion',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality kemis',
+    },
+
+    {
+      //_id: '2',
+      name: ' habeshawi habesha kemis',
+      slug: 'habeshawi-habesha-kemis',
+      category: 'kemis',
+      image: '/images/p3.jpg',
+      price: 5000,
+      countInStock: 0,
+      brand: 'habeshawi fashion',
+      rating: 4.5,
+      numReviews: 14,
+      description: 'high quality kemis',
+    },
+    {
+      //_id: '3',
+      name: ' mukash shifon',
+      slug: 'mukash-shifon',
+      category: 'kemis',
+      image: '/images/p4.jpeg',
+      price: 3000,
+      countInStock: 20,
+      brand: 'mukash fashion',
+      rating: 8.5,
+      numReviews: 13,
+      description: 'high quality kemis',
+    },
+    {
+      //_id: '4',
+      name: 'habeshawi kaba',
+      slug: 'habeshawi kaba',
+      category: 'kaba',
+      image: '/images/p2.jpg',
+      price: 5000,
+      countInStock: 10,
+      brand: 'mukash fashion',
+      rating: 6.5,
+      numReviews: 15,
+      description: 'high quality kaba',
+    },
+  ],
+};
+export default data;
